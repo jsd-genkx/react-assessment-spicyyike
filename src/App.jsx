7b9757c872d0c07dae2aa1ff6440e590b4082dcb
Rosarin <zenpinn@gmail.com>
2025-08-06 12:05:30 +0700

    checkpoint morning

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./components/Layout";
+import Home from "./views/Home";
+import Owner from "./views/Owner";
+
+const router = createBrowserRouter([{
+path: "/",
+element: <Layout />,
+    errorElement: (<div>
+      <h1 className="text-4xl p-8 text-center">404 Page Not Found</h1>
+    </div>),
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+            {
+        path: "owner",
+        element: <Owner />
+      }
+    ]
+}])
+
 export default function App() {
   return (
-    <div className="min-h-screen flex justify-center bg-blue-950">
-      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
-          React App Starter
-        </h1>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <ul className="list-inside list-disc flex-1">
-            <span className="font-semibold">Tech Stack:</span>
-            <li>Vite</li>
-            <li>React</li>
-            <li>JavaScript</li>
-            <li>Tailwind</li>
-          </ul>
-        </section>
-      </div>
+    <div className="min-h-screen flex justify-center bg-gray-300 w-full">
+      <RouterProvider router={router} />
     </div>
   );
 }
